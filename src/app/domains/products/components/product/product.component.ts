0fc4b732df206dbdc6d00df658f79228089df704
Jimena Silva <dev.jimena.silva@gmail.com>
2024-09-08 21:08:21 -0300

    add Product Components

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-product',
@@ -9,10 +10,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ProductComponent {
 
-  @Input({required: true}) img: string = '';
-  @Input({required: true}) price: number = 0;
-  @Input({required: true}) title: string = '';
-
+  @Input({required: true}) product!: Product;
   @Output() addToCart = new EventEmitter();
 
   addToCartHandler(){
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from './../../components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
@@ -9,6 +10,29 @@ import { ProductComponent } from './../../components/product/product.component';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([])
+
+  constructor(){
+    const initProducts:Product[] = [
+      {
+        id: Date.now(),
+        title: 'Producto 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 2',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=12',
+        creationAt: new Date().toISOString()
+      }
+    ]
+
+    this.products.set(initProducts)
+  }
   
   fromChild(event: string){
     
