38bbbcf8bc8b0c0957144617829f8c9e89ac3dc3
Jimena Silva <dev.jimena.silva@gmail.com>
2024-09-09 17:39:19 -0300

    add nested views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,23 @@
 import { Routes } from '@angular/router';
-import { ListComponent } from './domains/products/pages/list/list.component';
+import { ListComponent } from '@products/pages/list/list.component';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
 import { AboutComponent } from './domains/info/pages/about/about.component';
 import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: ListComponent
-    },
-    {
-        path: 'about',
-        component: AboutComponent
+        component: LayoutComponent,
+        children:[
+            {
+                path: '',
+                component: ListComponent
+            },
+            {
+                path: 'about',
+                component: AboutComponent
+            }
+        ]
     },
     {
         path: '**',
