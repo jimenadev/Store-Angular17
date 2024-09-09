85dea2995fa2d6e62f7ffdb1a5a4943ed8b7515a
Jimena Silva <dev.jimena.silva@gmail.com>
2024-09-09 16:28:03 -0300

    add pipes

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Product } from '@shared/models/product.model';
+import { TimeAgoPipe  } from '@shared/pipes/time-ago.pipe';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TimeAgoPipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
