ade452ac00c70963fc4eae3c41bc674c89716073
Jimena Silva <dev.jimena.silva@gmail.com>
2024-09-09 17:44:52 -0300

    add router link active

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
-import { RouterLinkWithHref } from '@angular/router';
+import { RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLinkWithHref],
+  imports: [RouterLinkWithHref, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
